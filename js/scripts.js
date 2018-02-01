0492cb467c272cf3e03c9f8e2d1d8d2258ae7c9f
Sean McDermott and Thomas Doherty <thomas@>
2018-01-31 20:30:19 -0800

    Create BMI calculator function.

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,37 @@ var add = function(number1, number2) {
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
+
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
 alert(result);
+
+var bmi = function(height, weight) {
+  return weight / height^2;
+};
+
+var height = parseInt(prompt("Enter your height:"));
+var weight = parseInt(prompt("Enter your weight:"));
+var total = bmi(height, weight);
+alert(total);
